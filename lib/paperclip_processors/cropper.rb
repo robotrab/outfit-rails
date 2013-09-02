6c83171661f56de68f76d36d91ca9ad3128f01c4
Rob Brown <rabrown.mail@gmail.com>
2013-09-02 09:59:40 -0400

    working on cropping

--- lib/paperclip_processors/cropper.rb
+++ lib/paperclip_processors/cropper.rb
@@ -1,30 +1,18 @@
 module Paperclip
   class Cropper < Thumbnail
     def transformation_command
-      if @options[:geometry] == "profile"
-        profile_crop
-      elsif @options[:geometry] == "mobile"
-        mobile_crop
+      if crop_command
+        crop_command + super.sub(/ -crop \S+/, '')
       else
         super
       end
     end
 
-    def profile_crop
+    def crop_command
       target = @attachment.instance
       if target.cropping?
-        ["-crop", "#{target.crop_w}x#{target.crop_h}+#{target.crop_x}+#{target.crop_y}", "+repage", "-resize", "364x646"]
-      else
-        ["-resize", "364x646"]
-      end
-    end
-
-    def mobile_crop
-      target = @attachment.instance
-      if target.cropping?
-        ["-crop", "#{target.crop_w}x#{target.crop_h}+#{target.crop_x}+#{target.crop_y}", "+repage", "-resize", "256x455"]
-      else
-        ["-resize", "256x455"]
+        puts '$$$$$$$$$$$$$$$$$$$PAPERCLIP PROCESSING$$$$$$$$$$$$$$$$$$$$$$'
+        " -crop #{target.crop_w}x#{target.crop_h}+#{target.crop_x}+#{target.crop_y}"
       end
     end
   end
