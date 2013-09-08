3d3cb32fc36f8c393becbf41d37b50d83b9dd7f0
Rob Brown <rabrown.mail@gmail.com>
2013-09-07 21:57:02 -0400

    Adding coordinates to form on click.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,16 +1,4 @@
 module PostsHelper
-    # def link_to_remove_tag(name, f)
-    #     f.hidden_field(:_destroy) + link_to_function(name, "remove_tag(this)")
-    # end
-
-    # def link_to_add_fields(name, f, association)
-    #     new_object = f.object.class.reflect_on_association(association).klass.new
-    #     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-    #         render(association.to_s.singularize + "_fields", :f => builder)
-    #     end
-    #     link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
-    # end
-
     def link_to_add_fields(name, f, association)
         new_object = f.object.send(association).klass.new
         id = new_object.object_id
